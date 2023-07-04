a047ed7d3089279d0f6bce23d18a362a2ca57f0b
BoiseITGuru <dev@boiseitguru.com>
2023-07-03 22:19:27 -0600

    package rename and .gitignore update

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
 export * from "./types";
-export { default as WarrantClient } from "./WarrantClient";
+export { default as Auth4FlowClient } from "./Auth4FlowClient";
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,33 @@
 const path = require("path");
 
-module.exports = env => {
-    return {
-        entry: "./src/index.ts",
-        resolve: {
-            extensions: [".ts", ".tsx", ".js"]
-        },
+module.exports = (env) => {
+  return {
+    entry: "./src/index.ts",
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"],
+    },
 
-        module: {
-            rules: [
-                // All files with a .ts or .tsx extension will be handled by ts-loader.
-                {
-                    test: /\.(ts|tsx)?$/,
-                    loader: "ts-loader",
-                    exclude: /node_modules/,
-                },
-            ]
+    module: {
+      rules: [
+        // All files with a .ts or .tsx extension will be handled by ts-loader.
+        {
+          test: /\.(ts|tsx)?$/,
+          loader: "ts-loader",
+          exclude: /node_modules/,
         },
+      ],
+    },
 
-        output: {
-            library: {
-                name: "warrant-js",
-                type: "umd"
-            },
+    output: {
+      library: {
+        name: "auth4flow-js",
+        type: "umd",
+      },
 
-            filename: "index.js",
-            path: path.resolve(__dirname, "dist"),
-            globalObject: "this",
-            clean: true,
-        },
-    }
+      filename: "index.js",
+      path: path.resolve(__dirname, "dist"),
+      globalObject: "this",
+      clean: true,
+    },
+  };
 };
